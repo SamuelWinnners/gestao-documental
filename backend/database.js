0bb1811a8028d0ab9af74bbbc160b725f629e56a
Samuel <samuelscruz0@gmail.com>
2025-11-06 16:45:14 -0300

    Novas atualizações no .env e database

--- backend/database.js
+++ backend/database.js
@@ -1,18 +1,16 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 const pool = mysql.createPool({
-  host: isProduction ? process.env.DB_HOST : 'localhost',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
 
 export default pool;
